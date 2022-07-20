f11a1480bedbef80ea6a94d9a2b163a1418bb5f8
kstambaugh <kyle.w.stambaugh1@gmail.com>
2022-07-19 20:25:01 -0400

    adding home page view

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 require('dotenv').config()
-
 const { application } = require('express');
 const express = require('express');
 
 const app = express()
 
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 app.use('/places', require('./controllers/places'))
 
 app.get('/', function (req, res) {
-    res.send('hello world')
+    res.render('home')
 })
 app.get('*', (req, res) => {
     res.status(404).send('<h1>404 Page</h1>')
