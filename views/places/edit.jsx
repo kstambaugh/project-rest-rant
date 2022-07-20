5cd3b77ff8f03e87d01f1ef41e6bb9c232be2d77
kstambaugh <kyle.w.stambaugh1@gmail.com>
2022-07-19 20:52:53 -0400

    finished new, show and edit view pages

---
+++ views/places/edit.jsx
@@ -0,0 +1,14 @@
+const React = require('react')
+const Def = require('../default')
+
+function editForm() {
+    return (
+        <Def>
+            <main>
+                <h1>Edit Form</h1>
+            </main>
+        </Def>
+    )
+}
+
+module.exports = editForm
