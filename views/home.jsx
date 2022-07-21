724cacfdc791bb6b933ec216b5b60c4f342de6a8
kstambaugh <kyle.w.stambaugh1@gmail.com>
2022-07-21 19:34:56 -0400

    adding styles to REST-rant

--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,9 @@ function home() {
         <Def>
             <main>
                 <h1>HOME</h1>
+                <a href="/places">
+                    <button className='button-primary'>Places Page</button>
+                </a>
             </main>
         </Def>
     )
