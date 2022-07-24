de09d155a6b8d0678388acb8583608773fbc3114
kstambaugh <kyle.w.stambaugh1@gmail.com>
2022-07-24 11:34:14 -0400

    form data is getting through!

--- controllers/places.js
+++ controllers/places.js
@@ -1,29 +1,47 @@
 const router = require('express').Router()
 
-router.get('/', (req, res) => {
-    let places = [{
-        name: 'The Fuzzy Pumpkin',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Witches Brew',
-        pic: '/images/pumpkinsoup.png'
-    }, {
-        name: 'Meat Your Veggies',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Meat',
-        pic: '/images/banana.png'
-    }]
-    res.render('places/index', { places })
-})
+let places = [{
+    name: 'The Fuzzy Pumpkin',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Witches Brew',
+    pic: '/images/pumpkinsoup.png'
+}, {
+    name: 'Meat Your Veggies',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Meat',
+    pic: '/images/banana.png'
+}]
 
-router.get('/new', (req, res) => {
-    res.render('places/new')
-})
 router.post('/', (req, res) => {
     console.log(req.body)
     res.send('POST /places')
 })
+
+router.get('/', (req, res) => {
+
+})
+//new route
+router.get('/new', (req, res) => {
+    res.render('places/new')
+})
+
+router.get('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        res.render('places/show', { place: places[id] })
+    }
+
+    res.render('places/index', { places })
+})
+
 
 
 module.exports = router
